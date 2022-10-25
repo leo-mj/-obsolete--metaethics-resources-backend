d8f81dddaa24208dd4a0caee532d3408d393b156
Leo <leo.maedje@gmail.com>
2022-10-25 12:31:54 +0100

    Added selecting a specific id

--- api/[id].js
+++ api/[id].js
@@ -1,12 +1,11 @@
 import connectToDB from "./utils/connectToDB";
 
-export default async function addResource(req, res) {
-    const {title, url, description} = req.body;
+export default async function getSingleResource(req, res) {
+    const {id} = req.query;
     const supabase = connectToDB();
     const {data, error} = await supabase
         .from('resources')
-        .insert({title: title, url: url, description: description})
-        .select();
+        .select('id', id)
     if (error) {
         return res.status(400).send(error);
     }

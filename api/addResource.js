09c918042a75c5a42d6ce0084959fe45594d3e4a
Leo <leo.maedje@gmail.com>
2022-10-25 12:20:46 +0100

    Wrote addResource

---
+++ api/addResource.js
@@ -0,0 +1,13 @@
+import connectToDB from "./utils/connectToDB";
+
+export default async function addResource(req, res) {
+    const {title, url, description} = req.body;
+    const supabase = connectToDB();
+    const {data, error} = await supabase
+        .from('resources')
+        .insert({title: title, url: url, description: description});
+    if (error) {
+        return res.status(400).send(error);
+    }
+    return res.status(200).send(data);
+}
